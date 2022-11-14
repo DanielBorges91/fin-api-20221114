efdbc0fa2538d6c877eb63771f0c77ef845a47f6
Daniel Borges <danprog91@gmail.com>
2022-11-14 18:02:58 -0300

    Validando a conta para gerar extrato

--- src/index.js
+++ src/index.js
@@ -33,6 +33,10 @@ app.get("/statement/:cpf", (request, response) => {
   const { cpf } = request.params;
   const customer = customers.find(customer => customer.cpf === cpf);
 
+  if(!customer) {
+    return response.status(400).json({ error: "Customer not exists!" });
+  }
+
   return response.json(customer.statement);
 
 });
