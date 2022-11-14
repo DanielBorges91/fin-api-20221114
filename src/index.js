853e744520e3a6144f8195f8b23c41df48f589c2
Daniel Borges <danprog91@gmail.com>
2022-11-14 19:07:20 -0300

    Criando depósito na conta

--- src/index.js
+++ src/index.js
@@ -49,6 +49,22 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { amount, description } = request.body;
+
+  const statementOperation = {
+    amount,
+    description,
+    created_at: new Date(),
+    type: "credit",
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
 app.listen(3333, () => {
   console.log("Servidor em execução!");
 });
