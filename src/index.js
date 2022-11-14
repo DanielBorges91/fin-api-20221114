46d4aa11b2be12dcc633b2367697c23722161a92
Daniel Borges <danprog91@gmail.com>
2022-11-14 17:37:56 -0300

    Cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const { v4: uuidV4} = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3333);
+const customers = [];
+
+app.put("/account", (request, response) => {
+  const { name, cpf} = request.body;
+
+  const account = {
+    id: uuidV4(),
+    name,
+    cpf,
+    created_at: new Date(),
+    statement: [],
+  }
+
+  customers.push(account);
+
+  return response.status(201).json(account);
+});
+
+app.listen(3333, () => {
+  console.log("Servidor em execução!");
+});
