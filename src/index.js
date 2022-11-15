e3e82627f530db9768d68709a86cd092f5f03314
Daniel Borges <danprog91@gmail.com>
2022-11-14 21:14:19 -0300

    Remover conta

--- src/index.js
+++ src/index.js
@@ -127,6 +127,14 @@ app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+})
+
 app.listen(3333, () => {
   console.log("Servidor em execução!");
 });
